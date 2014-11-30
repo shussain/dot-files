ffecb87ae868b1bec16622f2a291c489c3860b2f
Samir Hussain <shussain@credil.org>
2014-11-30 16:32:23 -0500

    Adding install/setup script

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ Must install and use stow to manage different packages
 
 xfce4-keyboard-shortcuts.xml is located at:
 $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
-
-oh-my-zsh git repo: https://github.com/robbyrussell/oh-my-zsh
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CURRENT_FOLDER=$PWD
+OH_MY_ZSH_DIR='.oh-my-zsh'
+
+echo "----- Install dependencies -----"
+sudo apt-get install exuberant-ctags stow
+
+echo "----- Update vimrc submodules -----"
+git submodule init; git submodule sync; git submodule update
+
+cd ..
+
+if [ ! -d "$OH_MY_ZSH_DIR" ]; then
+    echo "----- Get $OH_MY_ZSH_DIR submodules -----"
+    git clone https://github.com/robbyrussell/oh-my-zsh $OH_MY_ZSH_DIR
+fi
+
+cd "$CURRENT_FOLDER"
+
+echo "----- Stow configs and vimrc -----"
+stow -R configs
+stow -R vimrc
+
+echo "----- End -----"
