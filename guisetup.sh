2274801da05e9a34920da82218d4b59664691bcd
Samir Hussain <shussain@credil.org>
2016-06-11 17:52:51 -0400

    Updating GUI setup script - added redshift

--- guisetup.sh
+++ guisetup.sh
@@ -6,6 +6,7 @@ echo "----- Getting latest version -----"
 git pull
 
 echo "----- Install dependencies -----"
+sudo apt-get install redshift
 sudo apt-get install xdotool
 sudo apt-get install volumeicon-alsa
 
