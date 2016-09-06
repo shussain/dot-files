2c4b6e8bc16a14819e3d833f81e202af349f5edc
Samir Hussain <shussain@xelerance.com>
2016-09-06 12:42:59 -0400

    Update dev setup with dev tools

--- devsetup.sh
+++ devsetup.sh
@@ -3,9 +3,12 @@
 CURRENT_FOLDER=$PWD
 
 echo "----- Install necessary dev packages -----"
-sudo apt-get install gitk
 sudo apt-get install build-essential
 sudo apt-get install make
 sudo apt-get install gcc
+
+echo "----- Install useful dev tools -----"
+sudo apt-get install gitk
+sudo apt-get install tig
 
 cd "$CURRENT_FOLDER"
