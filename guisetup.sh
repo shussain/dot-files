4cf0351e76918a92ce8c14d2316aeda741d9e070
Samir Hussain <shussain@xelerance.com>
2016-10-12 15:55:35 -0400

    Add ssh-askpass for guisetup script
    ssh-askpass will have a GUI way to ask for passphrase
    when doing ssh-add    Very useful for start up script
    that invoke ssh-add without using a terminal

--- guisetup.sh
+++ guisetup.sh
@@ -11,5 +11,6 @@ sudo apt-get install xdotool
 sudo apt-get install volumeicon-alsa
 sudo apt-get install xautolock
 sudo apt-get install feh
+sudo apt-get install ssh-askpass
 
 cd "$CURRENT_FOLDER"
