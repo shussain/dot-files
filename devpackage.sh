9501f250672cca250cf92d5b86275b1961e143a7
Samir Hussain <shussain@xelerance.com>
2016-12-22 15:41:39 -0500

    Install meld when running devpackages script

--- devpackage.sh
+++ devpackage.sh
@@ -13,5 +13,6 @@ echo "----- Install useful dev tools -----"
 sudo apt-get install exuberant-ctags
 sudo apt-get install gitk
 sudo apt-get install tig
+sudo apt-get install meld
 
 cd "$CURRENT_FOLDER"
