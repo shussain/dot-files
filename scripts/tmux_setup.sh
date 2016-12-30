c2129147f61cf875acd4ebb715b9487aaaa80726
Samir Hussain <shussain@xelerance.com>
2016-12-30 00:03:51 -0500

    Remove htop from tmux_setup script (it is too distractign)

--- scripts/tmux_setup.sh
+++ scripts/tmux_setup.sh
@@ -3,11 +3,9 @@
 # Script for setting up Tmux
 tmux new-session -d
 tmux split-window -d -t 0 -v
-tmux split-window -d -t 0 -h
-tmux split-window -d -t 2 -h
+tmux split-window -d -t 1 -h
 
 tmux send-keys -t 0 'phonelist' enter
-tmux send-keys -t 2 'htop' enter C-l
 tmux send-keys -t 1 'vim' enter
 
 tmux select-pane -t 0
