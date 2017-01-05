b33682e9ac0d4fa6f5ca0d3ae37ac5d78ded00f5
Samir Hussain <shussain@xelerance.com>
2017-01-04 22:57:56 -0500

    Update tmux_setup.sh to setup tmux properly

--- scripts/tmux_setup.sh
+++ scripts/tmux_setup.sh
@@ -2,11 +2,11 @@
 
 # Script for setting up Tmux
 tmux new-session -d
-tmux split-window -d -t 0 -v
-tmux split-window -d -t 1 -h
+tmux split-window -d -t 0 -h
+#tmux split-window -d -t 1 -v
 
 tmux send-keys -t 0 'phonelist' enter
-tmux send-keys -t 1 'vim' enter
+#tmux send-keys -t 1 'vim' enter
 
 tmux select-pane -t 0
 
