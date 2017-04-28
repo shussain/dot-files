db2e34619b608b716344efca823b4ecec2193c61
Samir Hussain <shussain@xelerance.com>
2017-04-28 14:49:56 -0400

    Install thunar (file manager) if running guisetup.sh

--- guipackages.sh
+++ guipackages.sh
@@ -8,6 +8,7 @@ echo "----- Getting latest version -----"
 git pull
 
 echo "----- Install dependencies -----"
+sudo apt-get install thunar
 sudo apt-get install redshift
 sudo apt-get install xdotool
 sudo apt-get install volumeicon-alsa
