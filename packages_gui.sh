9300fa8bdc684c5823ae886b439716e634b98881
Samir Hussain <shussain@xelerance.com>
2018-10-25 21:05:58 -0400

    Add thunar dependencies

--- packages_gui.sh
+++ packages_gui.sh
@@ -10,12 +10,14 @@ git pull
 echo "----- Install useful GUI dependencies -----"
 sudo apt-get install arandr
 sudo apt-get install rdesktop
-sudo apt-get install thunar
 sudo apt-get install redshift
 sudo apt-get install feh
 sudo apt-get install cmus
 sudo apt-get install ssh-askpass
 sudo apt-get install gksu
+
+sudo apt-get install thunar
+sudo apt install tumbler tumbler-plugins-extra ffmpegthumbnailer
 
 sudo snap install tldr
 
