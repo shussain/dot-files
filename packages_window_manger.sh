4ae2d17fb65354da8659f29d39dd8c80af24ec60
Samir Hussain <shussain@xelerance.com>
2019-08-03 16:59:40 -0400

    Rearraning packages in packages_window_manger

--- packages_window_manger.sh
+++ packages_window_manger.sh
@@ -5,11 +5,9 @@
 CURRENT_FOLDER=$PWD
 
 echo "----- Install useful GUI dependencies -----"
-sudo apt-get install i3
-sudo apt-get install rofi
-sudo apt-get install blueman
-sudo apt-get install volumeicon-alsa
+sudo apt-get install i3 rofi
 sudo apt-get install xautolock
+sudo apt-get install blueman volumeicon-alsa
 
 cd "$CURRENT_FOLDER"
 echo "----- End -----"
