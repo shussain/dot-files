34e6c4f637ae5545b02c5e5781efa95230919400
Samir Hussain <shussain@xelerance.com>
2019-08-20 17:27:29 -0400

    Add nagstamon (NMS monitoring tool) to work install scripts

--- packages_work.sh
+++ packages_work.sh
@@ -6,6 +6,7 @@ CURRENT_FOLDER=$PWD
 
 echo "----- Install necessary work packages -----"
 sudo apt-get install hiera-eyaml
+sudo apt-get install nagstamon
 
 echo "----- Install useful dev tools -----"
 sudo apt-get install puppet-lint
