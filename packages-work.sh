da48bafc67bacaee90dc462305d7688dcc422cd9
Samir Hussain <shussain@xelerance.com>
2020-10-19 19:49:51 -0400

    Updating package script for work

--- packages-work.sh
+++ packages-work.sh
@@ -7,8 +7,6 @@ CURRENT_FOLDER=$PWD
 echo "----- Install necessary work packages -----"
 sudo apt-get install hiera-eyaml
 sudo apt-get install nagstamon
-
-echo "----- Install useful dev tools -----"
 sudo apt-get install puppet-lint
 
 cd "$CURRENT_FOLDER"
