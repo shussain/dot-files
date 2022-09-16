d33c5fa8bf0399f5d9931fe064c9722a3974bd3c
Samir Hussain <shussain@xelerance.com>
2022-09-16 17:29:20 -0400

    use vim for tmux setup script

--- scripts/tmux_setup.sh
+++ scripts/tmux_setup.sh
@@ -7,7 +7,7 @@ tmux new-session -d
 tmux split-window -d -t 0 -h
 tmux split-window -d -t 1 -v
 
-tmux send-keys -t 0 'phonelist' enter
+tmux send-keys -t 0 'nvim' enter
 tmux send-keys -t 1 'weather_short' enter
 tmux send-keys -t 2 'apt-dater' enter
 
